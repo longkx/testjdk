32af9de3f5dfd8fa7e90cb882353545e655935e3
longkaixiang <longkaixiang@bonc.com.cn>
2017-03-07 16:20:34 +0800

    testjdk

---
+++ src/main/java/com/testjdk/demo/Demo.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package com.testjdk.demo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Demo {
+	public static void main(String[] args) {
+		for (int i = 0; i==0; i++,i--) {
+			System.out.println(System.getProperty("java.version"));
+		}
+	}
+}
