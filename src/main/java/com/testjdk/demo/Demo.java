12c7e48583ae7d9e5ed4d0aaa267fea5a62da320
longkaixiang <longkaixiang@bonc.com.cn>
2017-03-08 08:50:04 +0800

    暂停10秒 2017年3月8日 08:50:01

--- src/main/java/com/testjdk/demo/Demo.java
+++ src/main/java/com/testjdk/demo/Demo.java
@@ -11,6 +11,12 @@ public class Demo {
 	public static void main(String[] args) {
 		for (int i = 0; i==0; i++,i--) {
 			System.out.println(System.getProperty("java.version"));
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
